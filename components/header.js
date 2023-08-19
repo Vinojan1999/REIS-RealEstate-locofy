224d7f1644db17e24dabb72825549a8ff92d9b1c
Vinojan1999 <Vinojan1999>
2023-08-19 16:22:08 +0000

    First Commit from Locofy Builder

---
+++ components/header.js
@@ -0,0 +1,56 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="self-stretch bg-gray-white h-[98px] flex flex-row py-[22px] px-20 box-border items-center justify-center sticky w-full top-[0] [background:white] z-[2] text-center text-5xl text-primary-500 font-body-regular-600 lg:pl-10 lg:pr-10 lg:box-border md:pl-6 md:pr-6 md:box-border">
+      <div className="flex-1 flex flex-row items-center justify-between">
+        <a className="[text-decoration:none] flex flex-row items-center justify-center gap-[8px] text-[inherit]">
+          <img className="relative w-11 h-11" alt="" src="/houseline.svg" />
+          <div className="flex flex-col items-start justify-start">
+            <div className="relative leading-[24px] font-semibold">REIS</div>
+            <div className="relative text-sm leading-[16px] font-medium">
+              Real State
+            </div>
+          </div>
+        </a>
+        <div className="flex flex-row items-center justify-end gap-[36px] text-sm text-primary-900 sm:flex">
+          <div className="flex flex-row items-start justify-start gap-[30px] lg:flex md:hidden sm:hidden">
+            <a className="[text-decoration:none] relative leading-[22px] font-semibold text-[inherit]">
+              HOME
+            </a>
+            <a className="[text-decoration:none] relative leading-[22px] text-[inherit]">
+              ABOUT US
+            </a>
+            <Link
+              className="cursor-pointer [text-decoration:none] relative leading-[22px] text-[inherit]"
+              href="/"
+            >
+              PROPERTIES
+            </Link>
+            <a className="[text-decoration:none] relative leading-[22px] text-[inherit]">
+              GALLERY
+            </a>
+            <a className="[text-decoration:none] relative leading-[22px] text-[inherit]">
+              BLOG
+            </a>
+            <a className="[text-decoration:none] relative leading-[22px] text-[inherit]">
+              CONTACT US
+            </a>
+            <a className="[text-decoration:none] relative leading-[22px] text-[inherit]">
+              SEARCH
+            </a>
+          </div>
+          <button className="cursor-pointer [border:none] p-0 bg-[transparent] hidden flex-row items-center justify-center lg:hidden md:flex sm:flex">
+            <img
+              className="relative w-6 h-6 overflow-hidden shrink-0 md:flex"
+              alt=""
+              src="/notification.svg"
+            />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
